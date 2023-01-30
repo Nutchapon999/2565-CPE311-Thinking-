22c836219aeb6899856bcfe7f70a607eef53f517
Nutchapon999 <118237511+Nutchapon999@users.noreply.github.com>
2023-01-30 19:47:20 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -6,8 +6,11 @@
 #include "stm32l1xx_ll_utils.h"
 #include "stm32l1xx_ll_bus.h"
 #include "stm32l1xx_ll_usart.h"
-
+#include "stm32l1xx_ll_tim.h"
+#include "math.h"
+#include "stdio.h"
 #include "string.h"
+#include "stdlib.h"
 
 /*uint8_t usr_button = 0;
 unsigned int state = 0;*/
@@ -15,16 +18,49 @@ void GPIO_USART_Configure(void);
 void SystemClock_Config(void);
 void USART_Configure(void);
 void USART_SendString(uint8_t*, uint8_t);
+void SystemClock_Config(void);
+void GPIO_Cofig(void);
+void TIMX_IC_Config(void);
+
+uint16_t uwIC1 = 0;
+uint16_t uwIC2 = 0;
+uint16_t uwDiff = 0;
+uint16_t uhICIndex = 0;
+uint32_t TIM2CLK;
+uint32_t PSC;
+uint32_t IC1PSC;
+int distant, i;
 
 uint8_t recv_buffer[10];
 uint8_t idx = 0;
+uint8_t test[]="";
 
 int main()
  {
-		uint8_t test[] = "Hello! TeraTerm...";
+		char text[4];
 		USART_Configure();
-		USART_SendString(test,sizeof(test));
-		while(1);
+		//USART_SendString(test,sizeof(test));
+		while(1){
+		LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_2);
+		LL_mDelay(1);
+		LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_2);
+		if(uhICIndex == 2){
+			//Period Calculation
+			PSC = LL_TIM_GetPrescaler(TIM2) + 1;
+			TIM2CLK = SystemCoreClock / PSC;
+			IC1PSC = __LL_TIM_GET_ICPSC_RATIO(LL_TIM_IC_GetPrescaler(TIM2, LL_TIM_CHANNEL_CH1));
+			
+			//period = (uwDiff*(PSC) * 1.0) / (TIM2CLK * IC1PSC * 1.0);
+			//distant = (pow(10, 2)*340*period)/2;
+		  distant = ((uwDiff*340.0*pow(10,2))/2.0)/SystemCoreClock;
+			sprintf(text, "%02d", distant);  // Format the integer as a two-digit string
+			
+			//**convert string or int to uint8_t array**
+			
+			//USART_SendString(test,sizeof(test));
+			
+		}
+		}
 }
 void USART_SendString(uint8_t* str, uint8_t size)
 {
@@ -84,7 +120,75 @@ void USART_Configure(void)
     LL_USART_Init(USART2, &usart_conf);
     LL_USART_Enable(USART2);
 }
+void TIM2_IRQHandler(void){
+	if(LL_TIM_IsActiveFlag_CC2(TIM2) == SET){
+		LL_TIM_ClearFlag_CC2(TIM2);
+		//Detect 1st risinng edge
+		if(uhICIndex == 0){
+			uwIC1 = LL_TIM_IC_GetCaptureCH2(TIM2);
+			uhICIndex = 1;
+		} else if(uhICIndex == 1){
+			uwIC2 = LL_TIM_IC_GetCaptureCH2(TIM2);
+			if(uwIC2 > uwIC1)
+				uwDiff = uwIC2 - uwIC1;
+			else if(uwIC2 < uwIC1)
+				uwDiff = ((LL_TIM_GetAutoReload(TIM2) - uwIC1) + uwIC2) + 1;
+			uhICIndex = 2;
+		}
+	}
+}
 
+void TIMX_IC_Config(void){
+	LL_TIM_IC_InitTypeDef timic;
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
+	//TIM_IC Configure CH1
+	timic.ICActiveInput = LL_TIM_ACTIVEINPUT_DIRECTTI;
+	timic.ICFilter = LL_TIM_IC_FILTER_FDIV1_N2;
+	timic.ICPolarity = LL_TIM_IC_POLARITY_BOTHEDGE;
+	timic.ICPrescaler = LL_TIM_ICPSC_DIV1;
+	LL_TIM_IC_Init(TIM2, LL_TIM_CHANNEL_CH2, &timic);
+	
+	NVIC_SetPriority(TIM2_IRQn, 0);
+	
+	NVIC_EnableIRQ(TIM2_IRQn);
+	LL_TIM_EnableIT_CC2(TIM2);
+	LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH2);
+	LL_TIM_EnableCounter(TIM2);
+}
+
+void GPIO_Cofig(void){
+	LL_GPIO_InitTypeDef timic_gpio;
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+	//GPIO_Config
+	timic_gpio.Pin = LL_GPIO_PIN_2;
+	timic_gpio.Mode = LL_GPIO_MODE_OUTPUT;
+	timic_gpio.Pull = LL_GPIO_PULL_NO;
+	timic_gpio.OutputType = LL_GPIO_OUTPUT_PUSHPULL ;
+	timic_gpio.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+	LL_GPIO_Init(GPIOA,&timic_gpio);
+	
+	timic_gpio.Mode = LL_GPIO_MODE_ALTERNATE;
+	timic_gpio.Alternate = LL_GPIO_AF_1;
+	timic_gpio.Pin = LL_GPIO_PIN_1;
+	LL_GPIO_Init(GPIOA,&timic_gpio);
+	
+	//configure ltc4727js
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
+	
+	LL_GPIO_InitTypeDef ltc4727_initstruct;
+	ltc4727_initstruct.Mode = LL_GPIO_MODE_OUTPUT;
+	ltc4727_initstruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	ltc4727_initstruct.Pull = LL_GPIO_PULL_NO;
+	ltc4727_initstruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+	ltc4727_initstruct.Pin = LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 |
+													 LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15;
+	LL_GPIO_Init(GPIOB, &ltc4727_initstruct);
+	
+	ltc4727_initstruct.Pin = LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 |LL_GPIO_PIN_3;
+	LL_GPIO_Init(GPIOC, &ltc4727_initstruct);
+
+}
 void SystemClock_Config(void)
 {
   /* Enable ACC64 access and set FLASH latency */ 
