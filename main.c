f147cfcb3d0538c7c71d050b7292727972741481
Ornarin Pridiphetpong <118236681+Ornarin@users.noreply.github.com>
2023-02-06 16:56:50 +0700

    main.c_ver2.1

--- main.c
+++ main.c
@@ -26,13 +26,47 @@ void Speaker_Config(void); //Speaker Module
 uint8_t recv_buffer[10];
 uint8_t idx = 0;
 
+uint16_t uwIC1 = 0;
+uint16_t uwIC2 = 0;
+uint16_t uwDiff = 0;
+uint16_t uwICIndex = 0;
+
+uint32_t TIM2CLK;
+uint32_t PSC;
+uint32_t IC1PSC;
+
+float period =0.0;
+
 int main()
  {
+	 SystemClock_Config();
+	 GPIO_USART_Configure();
+	 TCS3200_Config();
+	 Ultrasonic_Config();
+	 
+	 LL_GPIO_ResetOutputPin(GPIOB,LL_GPIO_PIN_4);
+   LL_GPIO_ResetOutputPin(GPIOB,LL_GPIO_PIN_5);
+	 
 	  /*call function*/
-		uint8_t test[] = "Hello! TeraTerm...";
-		USART_Configure();
-		USART_SendString(test,sizeof(test));
-		while(1);
+//		uint8_t test[] = "Hello! TeraTerm...";
+//		USART_Configure();
+//		USART_SendString(test,sizeof(test));
+		while(1)
+		{
+			  LL_GPIO_SetOutputPin(GPIOB,LL_GPIO_PIN_4);
+        LL_mDelay(1);
+        LL_GPIO_ResetOutputPin(GPIOB,LL_GPIO_PIN_4);
+        if(uwICIndex == 2)
+        {
+            PSC = LL_TIM_GetPrescaler(TIM2) + 1;
+            TIM2CLK = SystemCoreClock / PSC;
+            IC1PSC = __LL_TIM_GET_ICPSC_RATIO(LL_TIM_IC_GetPrescaler(TIM2,LL_TIM_CHANNEL_CH1));
+            
+            period = ((uwDiff * 340) / 2) / 32000000.0 ;
+            uwICIndex = 0;
+			
+		    }
+     }
 }
 void USART_SendString(uint8_t* str, uint8_t size)
 {
@@ -145,6 +179,45 @@ void USART_Configure(void)
 
     LL_USART_Init(USART2, &usart_conf);
     LL_USART_Enable(USART2);
+}
+void TIMx_IC_Config(void)
+{
+    LL_TIM_IC_InitTypeDef timic;
+    
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
+    
+    timic.ICActiveInput = LL_TIM_ACTIVEINPUT_DIRECTTI;
+    timic.ICFilter = LL_TIM_IC_FILTER_FDIV1_N2;
+    timic.ICPolarity = LL_TIM_IC_POLARITY_BOTHEDGE;
+    timic.ICPrescaler = LL_TIM_ICPSC_DIV1;
+    LL_TIM_IC_Init(TIM2, LL_TIM_CHANNEL_CH2, &timic);
+    NVIC_SetPriority(TIM2_IRQn, 0);
+    NVIC_EnableIRQ(TIM2_IRQn);
+    LL_TIM_EnableIT_CC2(TIM2);
+    LL_TIM_CC_EnableChannel(TIM2,LL_TIM_CHANNEL_CH2);
+    LL_TIM_EnableCounter(TIM2);
+}
+
+void TIM2_IRQHandler(void)
+{
+    if(LL_TIM_IsActiveFlag_CC2(TIM2) == SET)
+    {
+        LL_TIM_ClearFlag_CC2(TIM2);
+        if(uwICIndex == 0)
+        {
+            uwIC1 = LL_TIM_IC_GetCaptureCH2(TIM2);
+            uwICIndex = 1;
+        }
+        else if(uwICIndex ==1)
+        {
+            uwIC2 = LL_TIM_IC_GetCaptureCH2(TIM2);
+            if(uwIC2 > uwIC1)
+                uwDiff = uwIC2 - uwIC1;
+            else if(uwIC2 < uwIC1)
+                uwDiff = ((LL_TIM_GetAutoReload(TIM2)-uwIC1) + uwIC2) + 1;
+            uwICIndex = 2;
+        }
+    }
 }
 
 void SystemClock_Config(void)
